457f36968ceb264485c4677e3bb64961db2a5786
Paraschiv Olivia 60085 <g60085@APP.PDG.INTRANET.ESI-BRU.BE>
2023-09-25 14:56:09 +0200

    ajout classe Point

--- g60085.atl.ascii/src/main/java/model/Point.java
+++ g60085.atl.ascii/src/main/java/model/Point.java
@@ -1,4 +1,29 @@
 package model;
 
+
 public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) { //constructeur par copie
+        this(p.x, p.y);
+    }
+
+
+    public void move(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+
+
+    public double distanceTo(Point other) {
+        return Math.sqrt(Math.pow((other.x - this.x), 2) + Math.pow((other.y - this.y), 2));
+    }
 }
+
+
