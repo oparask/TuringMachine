ba778eda00f07ec9e391358693ab504105e96dea
Paraschiv Olivia 60085 <g60085@APP.PDG.INTRANET.ESI-BRU.BE>
2023-09-25 14:56:36 +0200

    ajout de la classe Circle

--- g60085.atl.ascii/src/main/java/model/Circle.java
+++ g60085.atl.ascii/src/main/java/model/Circle.java
@@ -1,4 +1,31 @@
 package model;
 
-public class Circle {
+public class Circle implements Shape {
+
+    private double radius;
+    private Point center;
+
+    public Circle(Point center, double radius) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException("radius must be positive, received: " + radius);
+        }
+
+        this.center = new Point(center); //copie défensiveplus elegante
+    }
+
+
+    void move(double dx, double dy){
+
+    }
+    boolean isInside(Point p){
+
+    }
+
+    char getColor(); {
+
+    }
+
+    void setColor(char color){
+
+    }
 }
