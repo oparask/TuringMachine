ffe487c688b44d3246d43db205ee119a14f3bfee
60085@etu.he2b.be <60085@etu.he2b.be>
2023-10-23 14:36:52 +0200

    creation du 2eme mini projet

---
+++ g60085.atl.bmr/src/main/java/com/example/g60085_atl_bmr/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.g60085_atl_bmr;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
